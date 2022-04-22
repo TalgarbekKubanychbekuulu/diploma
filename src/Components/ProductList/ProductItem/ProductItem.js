12e1ae20d37e9345728c056dda5fa0ff04c64d35
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-04-22 23:25:37 +0600

    create logotip

--- src/Components/ProductList/ProductItem/ProductItem.js
+++ src/Components/ProductList/ProductItem/ProductItem.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import classes from "./ProductItem.module.css";
 
+
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
       <img src={product.image} />
       <Link to={"/products/" + product.productId}>{product.title}</Link>
-      <div className={classes.price}>Mercedes{product.price}</div>
+      <div className={classes.price}><img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Lamborghini_Logo.svg/640px-Lamborghini_Logo.svg.png" alt="car" width="55px" height="55px" /> {product.price}</div>
+
+
     </div>
   );
 }

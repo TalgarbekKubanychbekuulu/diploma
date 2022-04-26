f0f7de9e4a1637b5fa6e0a912ad9b30341bc7d2e
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-04-26 23:25:27 +0600

    create NavToggle

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,75 +1,34 @@
 // import "./Nav.css";
-
-// function Nav() {
-//   return (
-//     <nav className="Nav">
-//       <Logo />
-// <head>
-//   <link
-//     href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&display=swap"
-//     rel="stylesheet"
-//   />
-// </head>
-
-//       <div className="car">Hypercar market </div>
-//       <ul>
-//         <NavItem url="/"active>Home</NavItem>
-//         <NavItem url="/">About</NavItem>
-//         <NavItem url="/">Contacts</NavItem>
-
-//         {/* <li>
-//           {" "}
-//           <a href="https://iconscout.com/icons/mercedes"  >
-//             Mercedes Icon
-//           </a>{" "}
-//         </li>
-//         <li>
-//           {" "}
-//           <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a>
-//         </li>{" "}
-//         <a href="https://iconscout.com">Iconscout</a>
-//         <li>
-//           <a href="/">Categories</a>
-//         </li> */}
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
-
 // import Logo from "../ui/Logo";
-// import "./Nav.css";
-// // import Logo from "../ui/Logo/Logo";
 // import NavItem from "./NavItem/NavItem";
 
 // function Nav() {
 //   return (
 //     <nav className="Nav">
-//       <Logo />
-//       <ul className="fref">
-
-//         <NavItem url="/">Home</NavItem>
-//         <NavItem url="/products">Products</NavItem>
-//         <NavItem url="/contacts">Contacts</NavItem>
-//         <NavItem url="/model">Model</NavItem>
-//         <NavItem url="/category">Category</NavItem>
-
-//       </ul>
+//       <div className="container">
+//         <Logo />
+//         <ul>
+//           <NavItem url="/">Home</NavItem>
+//           <NavItem url="/products">Products</NavItem>
+//           <NavItem url="/contacts">Contacts</NavItem>
+//           <NavItem url="/category">Category</NavItem>
+//         </ul>
+//       </div>
 //     </nav>
 //   );
 // }
 
 // export default Nav;
 
-import "./Nav.css";
+import classes from "./Nav.module.css";
 import Logo from "../ui/Logo";
 import NavItem from "./NavItem/NavItem";
+import NavToggle from "./NavToggle/NavToggle";
 
 function Nav() {
   return (
-    <nav className="Nav">
-      <div className="container">
+    <nav className={classes.Nav}>
+      <div className={classes.container}>
         <Logo />
         <ul>
           <NavItem url="/">Home</NavItem>
@@ -77,6 +36,7 @@ function Nav() {
           <NavItem url="/contacts">Contacts</NavItem>
           <NavItem url="/category">Category</NavItem>
         </ul>
+        <NavToggle />
       </div>
     </nav>
   );

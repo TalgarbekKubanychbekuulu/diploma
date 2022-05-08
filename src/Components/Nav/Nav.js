70c2cdccc13b710009e28d711ff8bf5aeecf4784
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-05-09 00:28:20 +0600

    create new search and button "buy"

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -20,25 +20,25 @@
 
 // export default Nav;
 
-import classes from "./Nav.module.css";
+// import classes from "./Nav.module.css";
 import Logo from "../ui/Logo";
-import NavItem from "./NavItem/NavItem";
-import NavToggle from "./NavToggle/NavToggle";
+// import NavItem from "./NavItem/NavItem";
+// import NavToggle from "./NavToggle/NavToggle";
 
 function Nav() {
   return (
-    <nav className={classes.Nav}>
-      <div className={classes.container}>
+    // <nav className={classes.Nav}>
+    //   <div className={classes.container}>
         <Logo />
-        <ul>
+        /* <ul>
           <NavItem url="/">Home</NavItem>
           <NavItem url="/products">Products</NavItem>
           <NavItem url="/contacts">Contacts</NavItem>
           <NavItem url="/category">Category</NavItem>
-        </ul>
-        <NavToggle />
-      </div>
-    </nav>
+        </ul> */
+        /* <NavToggle /> */
+    //   </div>
+    // </nav>
   );
 }
 

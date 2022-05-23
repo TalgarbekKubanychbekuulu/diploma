9950e6bf6118d8fee530f12dea01f242951f1f12
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-05-23 15:53:41 +0600

    corrected 1 style

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,20 +1,33 @@
-import "./Contacts.css"
+import "./Contacts.css";
 
 function Contacts() {
   return (
     <>
-     <form className="form">
-  <h2 className="con">CONTACT US</h2>
-  <p className="text" type="Name:"><input className="inas" placeholder="Write your name here.."></input></p>
-  <p className="text" type="Email:"><input className="inas" placeholder="Let us know how to contact you back.."></input></p>
-  <p className="text" type="Message:"><input className="inas" placeholder="What would you like to tell us.."></input></p>
-  <button className="but">Send Message</button>
-  <div className="feet">
-    
-    _<span className="seet" className="fa fa-phone"></span> +996 990 171 169____
-    <span className="seet" className="fa fa-envelope-o"></span> inter_Cars@gmail.com_
-  </div>
-</form>
+      <form className="form">
+        <h2 className="con">CONTACT US</h2>
+        <p className="text" type="Name:">
+          <input className="inas" placeholder="Write your name here.."></input>
+        </p>
+        <p className="text" type="Email:">
+          <input
+            className="inas"
+            placeholder="Let us know how to contact you back.."
+          ></input>
+        </p>
+        <p className="text" type="Message:">
+          <input
+            className="inas"
+            placeholder="What would you like to tell us.."
+          ></input>
+        </p>
+        <button className="but">Send Message</button>
+        <div className="feet">
+          _+996 990 171
+          169____
+          {" "}
+          inter_Cars@gmail.com_
+        </div>
+      </form>
     </>
   );
 }

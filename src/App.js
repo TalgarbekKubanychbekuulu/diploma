dea4ae41364c97c57284a1cf8038a3e1f5699427
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-06-29 01:57:10 +0600

    Deleted effects and added new pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import Contacts from "./pages/Contacts";
 import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
+import Layout from "./components/Layout/Layout";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
 import About from "./pages/About";
-import Buy from "./pages/Buy";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
 import Products2 from "./pages/Products2";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-
+import React from "react";
+import SignOut from "./pages/SignOut";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({ type: 'cart/restore' });
-  }, []);
-
   return (
     <div className="App">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/products/:productId" element={<Product />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:categoryId" element={<Category />} />
-
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/buy" element={<Buy />} />
           <Route path="/products2" element={<Products2 />} />
+
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/signout" element={<SignOut />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>

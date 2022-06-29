a4c7b61f8d80c8554d0229d01a7fc8de3531172f
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-06-29 20:55:01 +0600

    corrected the error and wrote it with the help of constant

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -2,33 +2,28 @@ import axios from "axios";
 
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
-const key = "AIzaSyBQQa7UTG9Qv9MvHXiMSDhMtt7IgUD0SAs";
-const signInUrl =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
-  key;
-const signUpUrl =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + key;
+const key = "AIzaSyDCoBep8DiHrKiE7DsV7PIyuQjmd1swQrQ";
+const signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + key;
+const signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + key;
 
-export const start = createAsyncThunk("auth/start", (data, thunkAPI) => {
-  thunkAPI.dispatch({ type: "auth/init" });
-  thunkAPI.dispatch({ type: "auth/loading" });
-  axios
-    .post(data.method === "signin" ? signInUrl : signUpUrl, {
-      email: data.email,
-      password: data.password,
-      returnSecureToken: true,
-    })
-    .then((response) => {
-      thunkAPI.dispatch({ type: "auth/success", payload: response.data });
-    })
-    .catch((error) => {
-      thunkAPI.dispatch({ type: "auth/error", payload: error.response.data });
-    });
+export const start = createAsyncThunk('auth/start', (data, thunkAPI) => {
+  thunkAPI.dispatch({ type: 'auth/init' });
+  thunkAPI.dispatch({ type: 'auth/loading' });
+  axios.post(data.method === 'signin' ? signInUrl : signUpUrl, {
+    email: data.email,
+    password: data.password,
+    returnSecureToken: true,
+  }).then(response => {
+    thunkAPI.dispatch({ type: 'auth/success', payload: response.data });
+  }).catch(error => {
+    thunkAPI.dispatch({ type: 'auth/error', payload: error.response.data });
+  });
 });
 
+const authInit = JSON.parse(localStorage.getItem('auth') ? localStorage.getItem('auth') : 'null');
 const authSlice = createSlice({
   name: "auth",
-  initialState: {
+  initialState: authInit ? authInit : {
     localId: null,
     idToken: null,
     error: null,
@@ -52,8 +47,8 @@ const authSlice = createSlice({
     error: (state, action) => {
       state.loading = false;
       state.error = action.payload.error.message;
-    },
-  },
+    }
+  }
 });
 
 export default authSlice.reducer;

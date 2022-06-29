33b179bb9c11bb85887ed61f2d03a2dafcfaa66e
TalgarbekKubanychbekuulu <TalgarbekKubanuchbekuulu@gmail.com>
2022-06-29 12:33:42 +0600

    added new imports to put pictures

--- src/data/Products.js
+++ src/data/Products.js
@@ -10,10 +10,18 @@ import cap from "../assets/cap2.jpg";
 import beg from "../assets/chemodan2.jpg";
 import wallet from "../assets/wallet.jpg";
 import racing from "../assets/racing2.jpg";
+import old1 from "../assets/old1.webp";
+import old2 from "../assets/old2.jpg";
+import old3 from "../assets/old3.jpg";
+import old4 from "../assets/old4.jpg";
+import old5 from "../assets/old5.jpg";
+import new1 from "../assets/new1.jpg";
+import new2 from "../assets/new2.jpg";
+import new3 from "../assets/new3.webp";
 
 const products = [
   {
-    image: productImage2,
+    image: new1,
     productId: "inter_cars",
     categoryId: "inter_cars2",
     title: "Aventador",
@@ -23,9 +31,39 @@ const products = [
   },
 
   {
+    image: new2,
+    productId: "inter_cars3",
+    categoryId: "inter_crs4",
+    title: "Lamborghini E-class",
+    price: 50000,
+    description: `Lamborghini Urus is the world's first sports crossover with a supercar soul and the functionality of an SUV. A four -liter V8 engine with a dual turbocharged capacity of 650 liters. With. And 850 Nm of torque provides acceleration from 0 to 100 km/h in just `,
+    h2: "AVENTADOR LP 780-4 ULTIMAE",
+  },
+
+  {
+    image: new3,
+    productId: "New-models5",
+    categoryId: "New-model6",
+    title: "Lamborghini ",
+    description:
+      "Sián FKP 37 is the first super sporter equipped with a V12 engine with a hybrid technology based on supercondacitors. The combination of power, beauty and the cult engine of the V12 in combination with an electric booster contains this unrivaled miracle of",
+    price: 90000,
+  },
+  
+  {
+    image: productImage2,
+    productId: "inter_cars7",
+    categoryId: "inter_cars8",
+    title: "Aventador",
+    price: 45000,
+    description: `DESIGNED TO PUSH BEYOND PERFORMANCE
+    Revolutionary thinking is at the heart of every idea from Automobili Lamborghini. Whether it is aerospace-inspired design or technologies applied to the naturally aspirated V12 engine or carbon-fiber structure, going beyond accepted limits is part of our philosophy..`,
+  },
+
+  {
     image: productImage,
-    productId: "Inter_Car",
-    categoryId: "inter_cars2",
+    productId: "Inter_Car9",
+    categoryId: "inter_cars10",
     title: "Lamborghini Urus",
     price: 50000,
     description: `Lamborghini Urus is the world's first sports crossover with a supercar soul and the functionality of an SUV. A four -liter V8 engine with a dual turbocharged capacity of 650 liters. With. And 850 Nm of torque provides acceleration from 0 to 100 km/h in just `,
@@ -34,17 +72,75 @@ const products = [
 
   {
     image: model,
-    productId: "New-models1",
-    categoryId: "New-models",
+    productId: "New-models11",
+    categoryId: "New-models12",
     title: "Sián FKP 37",
     description:
       "Sián FKP 37 is the first super sporter equipped with a V12 engine with a hybrid technology based on supercondacitors. The combination of power, beauty and the cult engine of the V12 in combination with an electric booster contains this unrivaled miracle of",
     price: 90000,
   },
   {
+    image: old1,
+    productId: "inter_cars13",
+    categoryId: "inter_cars14",
+    title: "Old Lamborghini",
+    price: 45000,
+    description: `DESIGNED TO PUSH BEYOND PERFORMANCE
+    Revolutionary thinking is at the heart of every idea from Automobili Lamborghini. Whether it is aerospace-inspired design or technologies applied to the naturally aspirated V12 engine or carbon-fiber structure, going beyond accepted limits is part of our philosophy..`,
+  },
+
+  {
+    image: old2,
+    productId: "Inter_Car15",
+    categoryId: "inter_cars16",
+    title: "Old Lamborghini ",
+    price: 50000,
+    description: `Lamborghini Urus is the world's first sports crossover with a supercar soul and the functionality of an SUV. A four -liter V8 engine with a dual turbocharged capacity of 650 liters. With. And 850 Nm of torque provides acceleration from 0 to 100 km/h in just `,
+    h2: "AVENTADOR LP 780-4 ULTIMAE",
+  },
+
+  {
+    image: old3,
+    productId: "New-models17",
+    categoryId: "New-model18",
+    title: "Old Lamborghini",
+    description:
+      "Sián FKP 37 is the first super sporter equipped with a V12 engine with a hybrid technology based on supercondacitors. The combination of power, beauty and the cult engine of the V12 in combination with an electric booster contains this unrivaled miracle of",
+    price: 90000,
+  },
+  {
+    image: old4,
+    productId: "inter_cars19",
+    categoryId: "inter_cars20",
+    title: "Old Lamborghini",
+    price: 45000,
+    description: `DESIGNED TO PUSH BEYOND PERFORMANCE
+    Revolutionary thinking is at the heart of every idea from Automobili Lamborghini. Whether it is aerospace-inspired design or technologies applied to the naturally aspirated V12 engine or carbon-fiber structure, going beyond accepted limits is part of our philosophy..`,
+  },
+
+  {
+    image: old5,
+    productId: "Inter_Car21",
+    categoryId: "inter_cars22",
+    title: "Old Lamborghini ",
+    price: 50000,
+    description: `Lamborghini Urus is the world's first sports crossover with a supercar soul and the functionality of an SUV. A four -liter V8 engine with a dual turbocharged capacity of 650 liters. With. And 850 Nm of torque provides acceleration from 0 to 100 km/h in just `,
+    h2: "AVENTADOR LP 780-4 ULTIMAE",
+  },
+
+  {
+    image: old3,
+    productId: "New-models23",
+    categoryId: "New-models24",
+    title: "Old Lamborghini",
+    description:
+      "Sián FKP 37 is the first super sporter equipped with a V12 engine with a hybrid technology based on supercondacitors. The combination of power, beauty and the cult engine of the V12 in combination with an electric booster contains this unrivaled miracle of",
+    price: 90000,
+  },
+  {
     image: jacket,
-    productId: "Black Jacket",
-    categoryId: "Black Jacket2",
+    productId: "Black Jacket25",
+    categoryId: "Black Jacket26",
     title: "Black Jacket",
     price: 45000,
     description: `DESIGNED TO PUSH BEYOND PERFORMANCE
